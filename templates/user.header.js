ce1eda107a5491a2a5b308e9590674326e8e8e28
Macmod <zz.mcmd@gmail.com>
2025-05-15 11:06:13 -0300

    Hmm?

--- templates/user.header.js
+++
@@ -1,5 +0,0 @@
-user_pref("font.name.serif.x-western", "DejaVu Sans");
-user_pref("signon.showAutoCompleteFooter", false);
-user_pref("signon.rememberSignons", false);
-user_pref("signon.formlessCapture.enabled", false);
-user_pref("signon.storeWhenAutocompleteOff", false);
